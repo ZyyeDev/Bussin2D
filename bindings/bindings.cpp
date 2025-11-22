e67c0e84d8ce7f1026130f7c6cd148fa58eec4d6
NyanjaxX <166942889+zaydenstuff@users.noreply.github.com>
2025-11-23 00:59:56 +0100

    change sleep func to be in bindings.h

--- bindings/bindings.cpp
+++ bindings/bindings.cpp
@@ -1,24 +1,9 @@
-#ifdef _WIN32
-    #include <windows.h>
-    #define sleep(ms) Sleep(ms)
-#else
-    #include <unistd.h>
-    #define sleep(ms) usleep(ms)
-#endif
-
 #include <bindings.h>
 #include "lua_window.h"
 #include "lua_graphics.h"
 #include "lua_audio.h"
 #include "lua_keyboard.h"
 #include "lua_mouse.h"
-
-static int lua_sleep(lua_State *L){
-    float m = static_cast<float> (luaL_checknumber(L,1));
-    sleep(m * 1000);
-
-    return 0;
-}
 
 void register_all_bindings(lua_State* L){
     lua_pushcfunction(L, lua_sleep);
--- bindings/bindings.h
+++ bindings/bindings.h
@@ -1,6 +1,21 @@
 #pragma once
 #include <lua.hpp>
 
+#ifdef _WIN32
+    #include <windows.h>
+    #define sleep(ms) Sleep(ms)
+#else
+    #include <unistd.h>
+    #define sleep(ms) usleep(ms)
+#endif
+
+static int lua_sleep(lua_State *L){
+    float m = static_cast<float> (luaL_checknumber(L,1));
+    sleep(m * 1000);
+
+    return 0;
+}
+
 void register_all_bindings(lua_State* L);
 void register_window_bindings(lua_State* L);
 void register_graphics_bindings(lua_State* L);
