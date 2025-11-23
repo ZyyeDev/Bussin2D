5dbc4afa93ef9aa9b19ebc0441e99f3ab0336df1
NyanjaxX <166942889+zaydenstuff@users.noreply.github.com>
2025-11-23 01:16:48 +0100

    use extern

---
+++ bindings/variables.cpp
@@ -0,0 +1,7 @@
+#include "variables.h"
+
+Renderer* g_renderer = nullptr;
+Window* g_window = nullptr;
+Audio* g_audio = nullptr;
+Keyboard* g_keyboard = nullptr;
+Mouse* g_mouse = nullptr;
--- bindings/variables.h
+++ bindings/variables.h
@@ -6,28 +6,8 @@
 #include "modules/keyboard/keyboard.h"
 #include "modules/mouse/mouse.h"
 
-#ifndef LUA_RENDERER
-    #define LUA_RENDERER
-
-    static Renderer* g_renderer = nullptr;
-#endif
-#ifndef LUA_WINDOW
-    #define LUA_WINDOW
-
-    static Window* g_window = nullptr;
-#endif
-#ifndef LUA_AUDIO
-    #define LUA_AUDIO
-
-    static Audio* g_audio = nullptr;
-#endif
-#ifndef LUA_KEYBOARD
-    #define LUA_KEYBOARD
-
-    static Keyboard* g_keyboard = nullptr;
-#endif
-#ifndef LUA_MOUSE
-    #define LUA_MOUSE
-
-    static Mouse* g_mouse = nullptr;
-#endif
+extern Renderer* g_renderer;
+extern Window* g_window;
+extern Audio* g_audio;
+extern Keyboard* g_keyboard;
+extern Mouse* g_mouse;
