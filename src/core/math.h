cb05fe11283a7c99fd6caa9eb4feb65e58518797
NyanjaxX <166942889+ZyyeDev@users.noreply.github.com>
2025-11-24 21:39:21 +0100

    Add more vector2 math

--- src/core/math.h
+++ src/core/math.h
@@ -1,10 +1,34 @@
 #pragma once
 
+#include <cmath>
+
 struct Vector2{
     float x, y;
 
     Vector2() : x(0) , y(0) {};
     Vector2(float x, float y) : x(x), y(y) {};
+
+    Vector2 operator+(const Vector2& other) const{
+        return Vector2(x + other.x, y + other.y);
+    }
+
+    Vector2 operator-(const Vector2& other) const{
+        return Vector2(x - other.x, y - other.y);
+    }
+
+    Vector2 operator*(float scalar) const{
+        return Vector2(x * scalar, y * scalar);
+    }
+
+    float length() const {
+        return std::sqrt((x*x) + (y*y));
+    }
+
+    Vector2 normalized() const {
+        float len = length();
+        if (len > 0) return Vector2(x / len, y / len);
+        return Vector2(0.0f,0.0f);
+    }
 };
 
 struct Color{
